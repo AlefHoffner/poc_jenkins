ccf0b14adc7e2282c5d74bde23d51f73caae17dc
= <=>
2021-05-15 18:02:02 -0300

    poc jenkins

---
+++ POC_Jenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POC_Jenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
